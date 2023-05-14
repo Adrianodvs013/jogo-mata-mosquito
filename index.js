5de3aef8c6935cc66a54357ad273d549ec9d50cb
Adriano Gomes <112133600+Adrianodvs013@users.noreply.github.com>
2023-05-14 02:58:51 -0300

    .

--- index.js
+++ index.js
@@ -12,21 +12,41 @@ function ajustaTamanhoPalcoJogo() {//Criei essa função e coloquei a logica den
 
 ajustaTamanhoPalcoJogo()
 
-function posicaoRandomica(){//Essa função encapsuloutodo este bloco
-    var posicaoX = Math.floor(Math.random() * largura)
-var posicaoY = Math.floor(Math.random() * altura)
+function posicaoRandomica() {//Essa função encapsuloutodo este bloco
+    var posicaoX = Math.floor(Math.random() * largura) - 90
+    var posicaoY = Math.floor(Math.random() * altura) - 90
 
-console.log(posicaoX, posicaoY)
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
 
-//Criar o elemento html
-var mosquito = document.createElement('img')
-mosquito.src = 'imagens/mosquito.png'
-mosquito.className = 'mosquito1'
-mosquito.style.left = posicaoX + 'px'
-mosquito.style.top = posicaoY + 'px'
-mosquito.style.position = 'absolute'
+    console.log(posicaoX, posicaoY)
 
-document.body.appendChild(mosquito)
+    //Criar o elemento html
+    var mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosquito.png'
+    mosquito.className = tamanhoAleatorio()
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+
+    document.body.appendChild(mosquito)
+
+    console.log(tamanhoAleatorio())
+}
+
+function tamanhoAleatorio() {
+    var classe = Math.floor(Math.random() * 3)
+
+    switch (classe) {
+        case 0:
+            return 'mosquito1'
+
+        case 1:
+            return 'mosquito2'
+
+        case 2:
+            return 'mosquito3'
+    }
 }
 
 
