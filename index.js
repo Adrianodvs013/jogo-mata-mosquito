19908cd08daa93b5793beddf02fc0fc1bf7c5ec9
Adriano Gomes <112133600+Adrianodvs013@users.noreply.github.com>
2023-05-14 01:44:01 -0300

    .

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-//Definição das dimensoes
-var altura = window.innerHeight
-var largura = window.innerWidth
 
-console.log(altura, largura)//Para ver n console se estao recuperando
+var altura = 0
+var largura = 0
+
+function ajustaTamanhoPalcoJogo() {//Criei essa função e coloquei a logica dentro.
+    //Esta é a logica inserida na função
+    var altura = window.innerHeight
+    var largura = window.innerWidth
+
+    console.log(altura, largura)//Para ver n console se estao recuperando
+}
+
+ajustaTamanhoPalcoJogo()
+
+
